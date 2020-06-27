7072a9da6d097575d9ac4f723befa2dddbf04fe5
liuping <liuping@liuping.win>
2020-06-27 20:27:29 +0800

    identity

--- main/main.go
+++ main/main.go
@@ -8,6 +8,8 @@ import (
 	"github.com/google/gopacket/pcap"
 	"net"
 	"os"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -19,7 +21,7 @@ func main() {
 				fmt.Println("Usage:\n -conn <interface> <username> <password>")
 				return
 			} else {
-				authentication(os.Args[2])
+				authentication(os.Args[2], os.Args[3], os.Args[4])
 			}
 
 		} else if os.Args[1] == "-interface" {
@@ -42,7 +44,7 @@ func dev() []string {
 	return result
 }
 
-func authentication(dev string) {
+func authentication(dev string, username string, passwd string) {
 	iface, err := net.InterfaceByName(dev)
 	if err != nil {
 		panic(err)
@@ -62,27 +64,39 @@ func authentication(dev string) {
 	defer handle.Close()
 
 	stop := make(chan int)
-	go readEap(handle, stop)
+	go readEap(handle, iface, stop, username, passwd)
 	defer close(stop)
 
+exter:
 	for {
 		select {
 		case _ = <-stop:
-			return
+			break exter
 		default:
 			broadcast(handle, iface)
+			time.Sleep(5 * time.Second)
 		}
 	}
-
+	fmt.Println("wait forever!")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	wg.Wait()
 }
 
-func readEap(handle *pcap.Handle, stop chan int) {
+func readEap(handle *pcap.Handle, iface *net.Interface, stop chan int, username string, passwd string) {
 	src := gopacket.NewPacketSource(handle, layers.LayerTypeEthernet)
 	in := src.Packets()
 	for {
 		var packet gopacket.Packet
 		select {
 		case packet = <-in:
+			ethLayer := packet.Layer(layers.LayerTypeEthernet)
+			if ethLayer == nil {
+				return
+			}
+			eth := ethLayer.(*layers.Ethernet)
+			srcMac := eth.SrcMAC
+
 			eapLayer := packet.Layer(layers.LayerTypeEAP)
 			if eapLayer == nil {
 				continue
@@ -94,10 +108,13 @@ func readEap(handle *pcap.Handle, stop chan int) {
 			case layers.EAPCodeRequest:
 				switch eap.Type {
 				case layers.EAPTypeIdentity:
+					sendUserName(handle, iface, srcMac, eap.Id, username)
 					break
 				case 0x66:
+					sendPasswd(handle, iface, srcMac, eap.Id, username)
 					break
 				default:
+					fmt.Println("unknown eap type:", eap.Type)
 					break
 				}
 				break
@@ -109,6 +126,7 @@ func readEap(handle *pcap.Handle, stop chan int) {
 				fmt.Println("eap failure")
 				break
 			default:
+				fmt.Println("unknown epa code:", eap.Code)
 				break
 			}
 		}
@@ -140,15 +158,15 @@ func broadcast(handle *pcap.Handle, iface *net.Interface) error {
 	return nil
 }
 
-func sendUserName(handle *pcap.Handle, iface *net.Interface, username string) error {
+func sendUserName(handle *pcap.Handle, iface *net.Interface, srcMac net.HardwareAddr, id uint8, username string) error {
 	eth := layers.Ethernet{
 		SrcMAC:       iface.HardwareAddr,
-		DstMAC:       net.HardwareAddr{0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+		DstMAC:       srcMac,
 		EthernetType: 0x888e,
 	}
 	eap := layers.EAP{
 		Code:     layers.EAPCodeResponse,
-		Id:       0x1,
+		Id:       id,
 		Length:   uint16(len([]byte(username))),
 		Type:     layers.EAPTypeIdentity,
 		TypeData: []byte(username),
@@ -168,6 +186,47 @@ func sendUserName(handle *pcap.Handle, iface *net.Interface, username string) er
 
 }
 
-func sendAuth(handle *pcap.Handle, iface *net.Interface, addr *net.IPNet) error {
+func sendPasswd(handle *pcap.Handle, iface *net.Interface, destMac net.HardwareAddr, id uint8, passwd string) error {
+	eth := layers.Ethernet{
+		SrcMAC:       iface.HardwareAddr,
+		DstMAC:       destMac,
+		EthernetType: 0x888e,
+	}
+	passwdByte := []byte{
+		0x10, 0x96, 0x8e, 0xb4, 0x94, 0x7a,
+		0x9a, 0xc2, 0x04, 0x66, 0x6f, 0xaf, 0x57, 0x1a,
+		0x6c, 0x3e, 0xd0, 0x79, 0x6c, 0x69, 0x75, 0x70,
+		0x69, 0x6e, 0x67, 0x2f, 0x5a, 0x41, 0x4f, 0x4e,
+		0x4c, 0x49, 0x4e, 0x45, 0x2e, 0x43, 0x4f, 0x4d,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x41, 0x34, 0x52, 0x54,
+		0x39, 0x44, 0x54, 0x47, 0x48, 0x4d, 0x52, 0x4d,
+		0x54, 0x55, 0x5a, 0x42, 0x54, 0x51, 0x56, 0x5a,
+		0x57, 0x34, 0x42, 0x52, 0x56, 0x57, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x4c, 0x53, 0x37, 0x51,
+		0x45, 0x59, 0x56, 0x54}
+
+	eap := layers.EAP{
+		Code:     layers.EAPCodeResponse,
+		Id:       id,
+		Length:   uint16(len(passwdByte)),
+		Type:     layers.EAPTypeIdentity,
+		TypeData: passwdByte,
+	}
+
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{
+		FixLengths:       true,
+		ComputeChecksums: true,
+	}
+	gopacket.SerializeLayers(buf, opts, &eth, &eap)
+	fmt.Println("send passwrod ", passwd)
+	if err := handle.WritePacketData(buf.Bytes()); err != nil {
+		return err
+	}
 	return nil
 }
